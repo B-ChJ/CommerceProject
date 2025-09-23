8dded2d7dfe0fe8a2343f0b0ace11994ec5fb870
B-ChJ <bchaejoo@gmail.com>
2025-09-23 16:44:56 +0900

    Product 클래스 재구성 - 속성 제거 후 List만 선언. - 상품 추가 메서드 addProducts 구현. - 상품 목록 출력하는 메서드 getProductInfo 구현

--- src/com/example/commerce/Product.java
+++ src/com/example/commerce/Product.java
@@ -1,6 +1,7 @@
 package com.example.commerce;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**개별 상품 정보
@@ -12,45 +13,23 @@ import java.util.List;
  */
 public class Product {
     // 1. 속성
-    String name;
-    String description;
-    int price;
-    int quantity;
+    private final List<String> products =  new ArrayList<>();
 
     // 2. 생성자
-    Product(String name, int price, String description, int quantity) {
-        if(name == null || price < 0 || quantity < 0) {
-            throw new IllegalArgumentException("적절하지 않은 입력입니다. 다시 정보를 확인해 주세요.");
-        }
-
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.quantity = quantity;
-    }
 
     // 3. 기능
-    public String getName() {
-        return name;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public int getPrice() {
-        return price;
-    }
-    public int getQuantity() {
-        return quantity;
+    public void addProducts(String name, String price, String description, String quantity) {
+        Collections.addAll(products, name, price, description, quantity);
     }
 
-    @Override
-    public String toString() {
-        String str = name + " | " + price + "원 | " + description + " | " + quantity;
-        return str;
+    public void getProductInfo(int index) {
+        System.out.println((index+1) + ". "
+                + products.get(index) + " | "
+                + products.get(index+1) + "원 | "
+                + products.get(index+2));
     }
 
-    public void setProducts(String name, int price, String description, int quantity) {
-        List<Product> products = new ArrayList<>();
-        products.add(new Product(name, price, description, quantity));
+    public int size() {
+        return products.size();
     }
 }
