50a38e01c0255f650055c0649744548bba82dccf
B-ChJ <bchaejoo@gmail.com>
2025-09-23 10:37:54 +0900

    Product Enum → 클래스로 형식 변경. Product 속성, 생성자, setter 메서드 작성

--- src/com/example/commerce/Product.java
+++ src/com/example/commerce/Product.java
@@ -3,12 +3,33 @@ package com.example.commerce;
 import java.util.ArrayList;
 import java.util.List;
 
-/**개별 상품 정보*/
-public enum Product {
-    NAME,
-    PRICE,
-    DESCRIPTION,
-    QUANTITY;
+/**개별 상품 정보
+ * Product 클래스는 개별 상품에 관한 이름, 설명, 가격, 재고수량 정보를 관리하는 클래스입니다.
+ * @param name 상품명
+ * @param price 상품의 가격
+ * @param description 상품 설명
+ * @param quantity 재고 수량*/
+public class Product {
+    // 1. 속성
+    String name;
+    String description;
+    int price;
+    int quantity;
 
-    Product(String name, int price, String description, int quantity) {}
+    // 2. 생성자
+    Product(String name, int price, String description, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    // 3. 기능
+    public void getProducts() {
+    }
+    public List<Product> setProducts(String name, int price, String description, int quantity) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(name, price, description, quantity));
+        return products;
+    }
 }
