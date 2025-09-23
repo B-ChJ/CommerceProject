428304b0fa028d7819397385f0e2db9d611ea390
B-ChJ <bchaejoo@gmail.com>
2025-09-23 17:10:58 +0900

    Main 클래스 출력 형태를 예시와 똑같이 변경. Main 클래스 - 0 입력 시 종료 안내 문구 출력 추가.

--- src/com/example/commerce/Main.java
+++ src/com/example/commerce/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("상품을 등록하시겠습니까? 종료를 원할 경우 exit를 입력하세요.\nEnter yes to console...");
         String name, description, price, quantity;
         Product product = new Product();
-        int number = 0;
+        int number = 1;
 
         while(!input.nextLine().equals("exit")){
 
@@ -22,15 +22,18 @@ public class Main {
             quantity = input.next();
 
             product.addProducts(name, price, description, quantity);
-            number++;
 
             System.out.println("새로운 상품을 추가하시겠습니까?\nexit 입력 시 프로그램 종료");
             input.nextLine();
         }
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
         for (int i = 0; i < product.size(); i=i+4) {
             System.out.print(number + ". ");
             product.getProductInfo(i);
+            number++;
         }
+        System.out.println("0. 종료 | 프로그램 종료");
+
 
         input.close();
     }
