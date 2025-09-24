5be48e87daa26ffaac0d7c6d8718657eb9cd6b22
B-ChJ <bchaejoo@gmail.com>
2025-09-24 20:31:29 +0900

    Lv 3. 객체 지향 설계를 적용해 상품 카테고리와 고객 관리를 클래스 기반으로 관리하기 ✅ [실시간 커머스 플랫폼 메인]에서 카테고리 이름이 목록으로 출력되도록 구현.

--- src/com/example/commerce/Category.java
+++ src/com/example/commerce/Category.java
@@ -1,36 +1,42 @@
 package com.example.commerce;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**Product 클래스를 리스트로 관리*/
-public class Category {
-    //1. 속성
-    private String categoryName;
-    private List<Product> products = new ArrayList<>();
-
-    //2. 생성자
-    public Category(String categoryName, Product... product) {
-        this.categoryName = categoryName;
-        for(Product p : product) {
-            products.add(p);
-        }
-    }
-    //3. 기능
-    public void initialCategory() {
-    Category electronics = new Category("electronics", new Product("Galaxy S25", "최신 안드로이드 스마트폰", 1200000, 100),
+public enum Category {
+    ELECTRONICS("전자제품", new Product("Galaxy S25", "최신 안드로이드 스마트폰", 1200000, 100),
             new Product("iPhone 16", "Apple의 최신 스마트폰", 1350000, 120),
             new Product("MacBook Pro", "M3 칩셋이 탑재된 노트북", 2400000, 70),
-            new Product("AirPods Pro", "노이즈 캔슬링 무선 이어폰", 350000, 56));
-
-    Category clothes = new Category("clothes", new Product("WoolSweater", "울 소재 스웨터", 34000, 200),
+            new Product("AirPods Pro", "노이즈 캔슬링 무선 이어폰", 350000, 56)),
+    CLOTHES("의류", new Product("WoolSweater", "울 소재 스웨터", 34000, 200),
             new Product("Shirt", "기본 하얀 와이셔츠", 10000, 489),
             new Product("T-shirt", "반팔 무지 티셔츠", 5000, 326),
-            new Product("Wide-pants", "통 넓은 슬랙스 바지", 27000, 175));
-
-    Category foods = new Category("foods", new Product("돼지국밥", "머릿고기, 깍두기 제공", 8500, 50),
+            new Product("Wide-pants", "통 넓은 슬랙스 바지", 27000, 175)),
+    FOODS("음식", new Product("돼지국밥", "머릿고기, 깍두기 제공", 8500, 50),
             new Product("베이컨 크림 파스타", "꾸덕촉촉한 크림과 짭짤한 베이컨의 조화", 13000, 85),
             new Product("중화비빔밥", "불맛 가득 건더기 듬뿍(계란후라이는 완숙)", 11000, 61),
             new Product("새우튀김우동", "뜨끈한 우동 위에 왕새우 튀김 2마리", 12000, 20));
+
+    //1. 속성
+    private final String categoryName;
+    private final List<Product> products = new ArrayList<>();
+
+    //2. 생성자
+    Category(String categoryName, Product... product) {
+        this.categoryName = categoryName;
+        products.addAll(Arrays.asList(product));
+    }
+
+    //3. 기능
+//Product 객체들을 반환하는 getter 메서드
+    public List<Product> getCategoryList(String categoryName) {
+        System.out.println(Category.valueOf(categoryName));
+        return products;
+    }
+    //카테고리 이름을 반환하는 getter 메서드
+    public String getCategoryName() {
+        return categoryName;
     }
 }
--- src/com/example/commerce/CommerceSystem.java
+++ src/com/example/commerce/CommerceSystem.java
@@ -9,26 +9,28 @@ import java.util.Scanner;
  * 상품을 관리
  * 사용자의 입력값을 처리하는 클래스*/
 public class CommerceSystem {
-    private List<Product> products =  new ArrayList<>();
+    //1. 속성
+    //private Category category = new Category();
+    //2. 생성자
     //3. 기능
-    //Product 클래스 객체 p를 CommerceSystem 클래스에서 List 내용으로 추가하는 setter 메서드
-    public void addProduct(Product p) {
-        Collections.addAll(products, p);
-    }
     //상품 목록 products의 값을 가져오는 메서드
-    public List<Product> getProductList() {
-        return products;
-    }
 
     public void start() {
         Scanner input = new Scanner(System.in);
 
         //do-while 반복문으로 일단 상품 목록 1회 출력 후 조건문 check
         do {
-            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-            for(int i=0; i<products.size(); i++) {
-                System.out.println((i+1) + ". " + products.get(i));
+            System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
+            for (Category c : Category.values()) {
+                System.out.println((c.ordinal()+1) + ". " + c.getCategoryName());
             }
+            System.out.println("0. 종료 | 프로그램 종료");
+            int numCategory = input.nextInt();
+            switch (numCategory) {
+                case 0: break;
+                case 1:
+            }
+
             System.out.println("0. 종료 | 프로그램 종료\n다시 전체 상품 목록을 조회하시겠습니까? (yes/0)");
         }while(!input.nextLine().equals("0"));
         input.close();
@@ -36,12 +38,12 @@ public class CommerceSystem {
         System.out.println("입력을 종료합니다.");
     }
     //상품 목록 중 한 상품의 정보와 재고 수량을 조회하는 메서드
-    public List<Product> getProductInfo(int index) {
-        if(index==0) {
-            throw new IllegalArgumentException("상품 번호는 1번부터 시작합니다.");
-        }
-        else System.out.println((index-1) + ". " + products.get(index-1) + " | " + products.get(index-1).quantity + "개");
-        return products;
-    }
+//    public List<Product> getProductInfo(int index) {
+//        if(index==0) {
+//            throw new IllegalArgumentException("상품 번호는 1번부터 시작합니다.");
+//        }
+//        else System.out.println((index-1) + ". " + category.getCategoryList().get(index-1) + " | " + category.getCategoryList().get(index-1).quantity + "개");
+//        return category.getCategoryList();
+//    }
 
 }
