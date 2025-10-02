8722879a960cc138e4f8999df130e60918327b2a
B-ChJ <bchaejoo@gmail.com>
2025-10-02 14:12:02 +0900

    CartItem Class 추가. - 장바구니에 담긴 상품 목록을 관리하는 Class Product에 작성했던 재고 수량 변경 메서드 삭제 → Category에 작성 예정.

---
+++ src/com/example/commerce/CartItem.java
@@ -0,0 +1,32 @@
+package com.example.commerce;
+
+public class CartItem {
+    //1. 속성
+    private Product product; // 주문하는 상품 정보
+    private int countOrder; // 주문수량
+
+    //2. 생성자
+    public CartItem(Product product, int countOrder) {
+        this.product = product;
+        this.countOrder = countOrder;
+    }
+    //3. 기능
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public int getCountOrder() {
+        return countOrder;
+    }
+    public void setCountOrder(int countOrder) {
+        this.countOrder = countOrder;
+    }
+    public String getProductInfo() {
+        return product.getName() + " | " + product.getPrice() +"원 | " + product.getDescription() + " | " + countOrder + "개";
+    }
+    public int getCartItemPrice() {
+        return product.getPrice()*countOrder;
+    }
+}
--- src/com/example/commerce/Product.java
+++ src/com/example/commerce/Product.java
@@ -18,7 +18,7 @@ public class Product {
     private String description;
     private int price;
     private int quantity;
-    private int count=0;
+    //private int count=0;
 
     // 2. 생성자
     public Product(String name, String description, int price, int quantity) {
@@ -27,21 +27,6 @@ public class Product {
         this.price = price;
         this.quantity = quantity;
     }
-    // 전자제품 상품
-    Product electronics1 = new Product("Galaxy S25", "최신 안드로이드 스마트폰", 1200000, 100);
-    Product electronics2 = new Product("iPhone 16", "Apple의 최신 스마트폰", 1350000, 120);
-    Product electronics3 = new Product("MacBook Pro", "M3 칩셋이 탑재된 노트북", 2400000, 70);
-    Product electronics4 = new Product("AirPods Pro", "노이즈 캔슬링 무선 이어폰", 350000, 56);
-    // 의류 상품
-    Product clothes1 = new Product("WoolSweater", "울 소재 스웨터", 34000, 200);
-    Product clothes2 = new Product("Shirt", "기본 하얀 와이셔츠", 10000, 489);
-    Product clothes3 = new Product("T-shirt", "반팔 무지 티셔츠", 5000, 326);
-    Product clothes4 = new Product("Wide-pants", "통 넓은 슬랙스 바지", 27000, 175);
-    // 음식 상품
-    Product foods1 = new Product("돼지국밥", "머릿고기, 깍두기 제공", 8500, 34);
-    Product foods2 = new Product("베이컨 크림 파스타", "꾸덕촉촉한 크림과 짭짤한 베이컨의 조화", 13000, 13);
-    Product foods3 = new Product("중화비빔밥", "불맛 가득 건더기 듬뿍(계란후라이는 완숙)", 11000, 8);
-    Product foods4 = new Product("새우튀김우동", "뜨끈한 우동 위에 왕새우 튀김 2마리", 12000, 3);
 
     // 3. 기능
     /**Getter Method*/
@@ -62,9 +47,9 @@ public class Product {
         return quantity;
     }
     //주문 수량을 int로 return
-    public int getCount() {
-        return count;
-    }
+//    public int getCount() {
+//        return count;
+//    }
 
     /**Setter Method*/
     //입력값 String name을 Product의 상품명으로 입력
@@ -84,9 +69,9 @@ public class Product {
         this.description = description;
     }
     //입력값 int count를 Product의 주문 수량으로 입력
-    public void setCount(int count) {
-        this.count = count;
-    }
+//    public void setCount(int count) {
+//        this.count = count;
+//    }
 
     // 출력되는 형태를 "상품명 | 가격 | 상품설명" 방식으로 고정한다.
     @Override
@@ -95,10 +80,5 @@ public class Product {
         String str = String.format("%s | %s원 | %s", name, df.format(price), description);
         return str;
     }
-    //주문 수량만큼 재고수량을 감소시키는 메서드
-    public int decreaseQuantity(int orderQuantity) {
-        if(orderQuantity > this.quantity) { throw new ArrayIndexOutOfBoundsException("재고 수량보다 많은 개수를 주문할 수 없습니다.");}
-        this.quantity -= orderQuantity;
-        return this.quantity;
-    }
+
 }
