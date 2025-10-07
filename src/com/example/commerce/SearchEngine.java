a04c7a01c05a16e0300a57912462c718296edb5f
B-ChJ <bchaejoo@gmail.com>
2025-10-07 20:59:31 +0900

    CH2 커머스 플랫폼 알고리즘 최적화 과제 시작. 1. 상품 검색 엔진 고도화 ✅ lv 1. 이진탐색 기본 구현  - SearchEngine Class 생성  - binarySearch 메서드(재귀, 반복문 방식) 구현

---
+++ src/com/example/commerce/SearchEngine.java
@@ -0,0 +1,44 @@
+package com.example.commerce;
+
+import java.util.List;
+
+public class SearchEngine {
+    private List<Product> sortedProducts;
+    Category category;
+
+    public SearchEngine(Category category) {
+        category.getCategoryList().stream().sorted().toList();
+    }
+
+    public Product binarySearchRecursive(String productName, int left, int right) {
+        if (left <= right) {
+            int middle = (left + right) / 2;
+            if(category.getCategoryList().get(middle).getName().equals(productName)) {
+                return sortedProducts.get(middle);
+            }
+            else if(category.getCategoryList().get(middle).getName().compareTo(productName) > 0) { // middle > target : middle 상품의 문자가 productName보다 사전상 뒷 순서에 있으면
+                right = middle - 1; // middle의 왼쪽으로 검색 범위 이동
+            }
+            else { left = middle + 1; } // middle < target : middle 상품의 문자가 productName보다 사전상 앞 순서에 있으면 오른쪽으로 검색 범위 이동
+        }
+        return binarySearchRecursive(productName, left, right);
+    }
+
+    public Product binarySearchIterative(String productName) {
+        int left = 0;
+        int right = productName.length() - 1;
+        Product result = null;
+        while(left <= right) {
+            int middle = (left + right) / 2;
+            if(category.getCategoryList().get(middle).getName().compareTo(productName) > 0) {
+                right = middle - 1;
+            }
+            else if (category.getCategoryList().get(middle).getName().compareTo(productName) < 0) {
+                left = middle + 1;
+            }
+            else { result = category.getCategoryList().get(middle); }
+        }
+        if(result == null) { throw new NullPointerException("상품을 찾지 못했습니다."); }
+        return result;
+    }
+}
